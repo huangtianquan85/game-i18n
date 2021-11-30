7917e150d0952219376824b2c48dc03becbad3d1
Megrez <68074108@qq.com>
2021-12-01 00:23:04 +0800

    update sources

--- server.go
+++ server.go
@@ -72,7 +72,6 @@ func translatesEditor(w http.ResponseWriter, r *http.Request) {
 		LEFT JOIN en 
 		ON m.keyhash = en.keyHash and m.valueHash = en.valueHash;
 	`
-
 	// query all useful rows
 	rows, err := DB.Query(queryCmd)
 	if err != nil {
@@ -170,30 +169,79 @@ func commitTranslate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func update(r *http.Request) error {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("update-sources read body error: %v", err)
+	}
+
+	// unmarshal
+	var f interface{}
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		return fmt.Errorf("update-sources unmarshal error: %v", err)
+	}
+
+	m := f.(map[string]interface{})
+
+	// query all rows
+	queryCmd := "SELECT `main`.`key`, `main`.`useful` from main;"
+	rows, err := DB.Query(queryCmd)
+	if err != nil {
+		return fmt.Errorf("query error: %v", err)
+	}
+
+	// begin context
+	tx, err := DB.Begin()
+	if err != nil {
+		return fmt.Errorf("context begin error: %v", err)
+	}
+
+	// find useless items
+	dbItems := make(map[string]bool)
+	for rows.Next() {
+		var key string
+		var useful int
+		rows.Scan(&key, &useful)
+		dbItems[key] = useful == 1
+
+		_, ok := m[key]
+		if (useful == 1) != ok { // 状态不一样
+			useful = 1 - useful
+			tx.Exec("UPDATE main SET main.useful=? WHERE main.keyHash=?", useful, StringMd5(key))
+			if err != nil {
+				return fmt.Errorf("update useful error: %v", err)
+			}
+		}
+	}
+
+	// insert to main
+	cmd := "INSERT ignore INTO main (`keyHash`, `key`, `valueHash`, `source`, useful) VALUES (?,?,?,?,?);"
+	for k, _ := range m {
+		if _, ok := dbItems[k]; !ok {
+			_, err = tx.Exec(cmd, StringMd5(k), k, StringMd5(""), "", 1)
+			if err != nil {
+				return fmt.Errorf("insert new item error: %v", err)
+			}
+		}
+	}
+
+	// commit
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("commit error: %v", err)
+	}
+
+	return nil
+}
+
 // 更新翻译需求
-func newSources(w http.ResponseWriter, r *http.Request) {
-	// rows, err := DB.Query(string(body))
-	// if err != nil {
-	// 	fmt.Fprintln(w, "query error", err)
-	// 	w.WriteHeader(500)
-	// 	return
-	// }
-
-	// defer rows.Close()
-
-	// infos := make([]Info, 0)
-	// for rows.Next() {
-	// 	i := new(Info)
-	// 	rows.Scan(&i.Origin, &i.Translate, &i.StackHash, &i.Stack, &i.Comment, &i.ScreenshotHash, &i.Version)
-	// 	infos = append(infos, *i)
-	// }
-
-	// data, err := json.Marshal(infos)
-	// if err != nil {
-	// 	fmt.Fprintln(w, "query error", err)
-	// 	w.WriteHeader(500)
-	// 	return
-	// }
+func updateSources(w http.ResponseWriter, r *http.Request) {
+	if err := update(r); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -205,7 +253,7 @@ func StartServer() {
 	http.HandleFunc("/commit-translate", commitTranslate)
 	http.HandleFunc("/translates", translates)
 	http.HandleFunc("/translates-editor", translatesEditor)
-	http.HandleFunc("/new-sources", newSources)
+	http.HandleFunc("/update-sources", updateSources)
 	http.HandleFunc("/", index)
 	http.ListenAndServe("0.0.0.0:8081", nil)
 }
